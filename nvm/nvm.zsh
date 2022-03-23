6fe941ead20b1f66b83cebddd1f6e44e5af95bdb
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-03-23 11:43:37 +0100

    feat: replace nvm with n

--- nvm/nvm.zsh
+++
@@ -1,3 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-  [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh"
-  [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm"
