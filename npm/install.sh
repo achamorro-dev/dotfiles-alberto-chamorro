31301b18ad0c37dd08382e920912b9453386740e
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-08-12 15:15:10 +0200

    feat: add lazygit to Brewfile

---
+++ npm/install.sh
@@ -0,0 +1,2 @@
+# Install language servers for neovim LSP
+npm i -g typescript typescript-language-server vscode-langservers-extracted
---
+++ nvim/install.sh
@@ -0,0 +1,7 @@
+# Download and install Nerd Font
+curl -L -o ~/Downloads/nerd-fonts.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip
+mkdir -p ~/Downlodas/nerd-fonts
+unzip ~/Downloads/nerd-fonts.zip -d ~/Downlodas/nerd-fonts
+cp ~/Downlodas/nerd-fonts/*.ttf ~/Library/Fonts
+rm -rf ~/Downlodas/nerd-fonts*
+
