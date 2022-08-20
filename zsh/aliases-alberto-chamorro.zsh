779afac09fdfdf3750a9ba6e668ab6a7bc6a166d
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-08-20 17:59:28 +0200

    feat: add alias for tmux

--- zsh/aliases-alberto-chamorro.zsh
+++ zsh/aliases-alberto-chamorro.zsh
@@ -4,13 +4,22 @@ alias w="cd $HOME/workspace"
 ## COMMANDS
 alias c="clear"
 alias co="code ."
-alias n="nvim"
+
+# NPM
 alias ns="npm run serve"
 alias nst="npm run start"
 alias nd="npm run dev"
 alias ni="npm i"
 alias nci="npm ci"
 alias nr="npm run"
+
+# IDE
 alias i="idea"
 alias ws="webstorm"
 alias st="studio"
+alias n="nvim"
+alias vim="nvim"
+alias vi="nvim"
+
+# TMUX
+alias t="tmux"
