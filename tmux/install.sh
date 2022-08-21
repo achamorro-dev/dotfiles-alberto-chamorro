77dd499f144fc8b1f03cba9d1f5176bd0e0f6ade
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-08-21 08:51:16 +0200

    feat: add tmux package manager and reset tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,3 +1,9 @@
+# Install tmux package manager
+TPM_DIRECTORY=~/.tmux/plugins/tpm 
+if [[ ! -d $TPM_DIRECTORY ]];then
+  git clone https://github.com/tmux-plugins/tpm $TPM_DIRECTORY
+fi
+
 # Copy tmux config
 rm -rf ~/.config/tmux ~/.tmux.conf
 ln -s ~/.dotfiles/plugins/dotfiles-alberto-chamorro/tmux/config/tmux/tmux.conf ~/.tmux.conf
