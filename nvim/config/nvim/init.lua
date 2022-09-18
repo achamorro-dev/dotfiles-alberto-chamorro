ad7db801f851367ea804b54bf9c1363c5252f205
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-09-18 07:59:54 +0200

    feat: add key_bindings setting Alt with K and J

--- nvim/config/nvim/init.lua
+++ nvim/config/nvim/init.lua
@@ -1,29 +1,29 @@
 -- insures packer is installed
 local execute = vim.api.nvim_command
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+	fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+	execute("packadd packer.nvim")
 end
 
-require('plugins')
-require('plugins.onedark')
-require('plugins.autopairs')
-require('plugins.colorizer')
-require('plugins.telescope')
-require('plugins.treesitter')
-require('plugins.lsp')
-require('plugins.toggleterm')
-require('plugins.nvim-tree')
-require('plugins.comment')
-require('plugins.gitsigns')
-require('plugins.lualine')
-require('plugins.bufferline')
-require('plugins.cmp')
+require("plugins")
+require("plugins.onedark")
+require("plugins.autopairs")
+require("plugins.colorizer")
+require("plugins.telescope")
+require("plugins.treesitter")
+require("plugins.lsp")
+require("plugins.toggleterm")
+require("plugins.nvim-tree")
+require("plugins.comment")
+require("plugins.gitsigns")
+require("plugins.lualine")
+require("plugins.bufferline")
+require("plugins.cmp")
+require("plugins.diffview")
 
-require('settings')
-require('keymaps')
-require('autocommands')
-
+require("settings")
+require("keymaps")
+require("autocommands")
