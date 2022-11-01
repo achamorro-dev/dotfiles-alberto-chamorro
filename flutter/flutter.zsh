3f4411f23f9875b58bc8b43fe14bea7ecade6921
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-11-01 20:18:39 +0100

    feat: add aliases for flutter

--- flutter/flutter.zsh
+++ flutter/flutter.zsh
@@ -1,1 +1,7 @@
 export PATH="$HOME/fvm/default/bin":$PATH
+
+alias fd="flutter devices"
+alias frd="flutter run --flavor development -t lib/main_development.dart"
+alias frs="flutter run --flavor staging -t lib/main_staging.dart"
+alias frp="flutter run --flavor production -t lib/main_production.dart"
+
