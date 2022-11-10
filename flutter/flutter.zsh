4428b80d21a6a9a895a5523de84a24cbcc3c7688
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-11-10 07:09:34 +0100

    feat: add flutter build_runner alias command

--- flutter/flutter.zsh
+++ flutter/flutter.zsh
@@ -4,4 +4,5 @@ alias fd="flutter devices"
 alias frd="flutter run --flavor development -t lib/main_development.dart"
 alias frs="flutter run --flavor staging -t lib/main_staging.dart"
 alias frp="flutter run --flavor production -t lib/main_production.dart"
+alias frb="flutter pub run build_runner build"
 
