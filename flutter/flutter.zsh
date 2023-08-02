d6b2ec81d673bbbcddf57a38002a663f4b50f78e
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-08-02 08:15:34 +0200

    feat: change flutter aliases with fl prefix

--- flutter/flutter.zsh
+++ flutter/flutter.zsh
@@ -1,8 +1,8 @@
 export PATH="$HOME/fvm/default/bin":"$HOME/.pub-cache/bin":$PATH
 
-alias fd="flutter devices"
-alias frd="flutter run --flavor development -t lib/main_development.dart"
-alias frs="flutter run --flavor staging -t lib/main_staging.dart"
-alias frp="flutter run --flavor production -t lib/main_production.dart"
-alias frb="flutter pub run build_runner build"
+alias fld="flutter devices"
+alias flrd="flutter run --flavor development -t lib/main_development.dart"
+alias flrs="flutter run --flavor staging -t lib/main_staging.dart"
+alias flrp="flutter run --flavor production -t lib/main_production.dart"
+alias flrb="flutter pub run build_runner build"
 
