504338914274bcd05638119025635384472ddb2a
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-01-10 06:39:35 +0100

    feat(app-store): add DemoPro

--- app-store/install.sh
+++ app-store/install.sh
@@ -1,7 +1,8 @@
-if test "$(uname)" = "Darwin"
-then
-  # Tomito
-  mas install 1526042938
-  # Unsplash Wallpapers
-  mas install 1284863847
+if test "$(uname)" = "Darwin"; then
+	# Tomito
+	mas install 1526042938
+	# Unsplash Wallpapers
+	mas install 1284863847
+	# DemoPRO
+	mas install 1384206666
 fi
