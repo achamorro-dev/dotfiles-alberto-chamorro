25ea21f8bdec3b464bdfff55f247b510b5999133
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-01-23 11:33:04 +0100

    fix: nvim debugger install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -11,12 +11,15 @@ if [ ! -d $CHROME_DEBUGGER_PATH ];then
 
   cd $CHROME_DEBUGGER_PATH &&
   npm install &&
-  npm run build
+  npm run build &&
+  cd -
 fi
 
 NODE_DEBUGGER_PATH=$HOME/.debuggers/vscode-js-debug
+NODE_DEBUGGER_VERSION=v1.85.0
 if [ ! -d $NODE_DEBUGGER_PATH ];then
   mkdir -p $NODE_DEBUGGER_PATH
-  wget https://github.com/microsoft/vscode-js-debug/releases/download/v1.85.0/js-debug-dap-v1.85.0.tar.gz -p /tmp
-  tar xvfz /tmp/js-debug-dap-v1.85.0.tar.gz -C $NODE_DEBUGGER_PATH
+  wget https://github.com/microsoft/vscode-js-debug/releases/download/$NODE_DEBUGGER_VERSION/js-debug-dap-$NODE_DEBUGGER_VERSION.tar.gz -P $HOME
+  tar xvfz $HOME/js-debug-dap-$NODE_DEBUGGER_VERSION.tar.gz -C $NODE_DEBUGGER_PATH
+  rm -f $HOME/js-debug-dap-$NODE_DEBUGGER_VERSION.tar.gz
 fi
