733957cba52bccd133e1c377cb36974ccdd128a7
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-01-23 06:52:09 +0100

    feat(aliases): add movements aliases

--- zsh/aliases-alberto-chamorro.zsh
+++ zsh/aliases-alberto-chamorro.zsh
@@ -43,3 +43,10 @@ alias llt="exa -1 --icons --tree --git-ignore"
 
 # Chrome CORS disabled
 alias ccors="open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir=\"/tmp/chrome_dev_test\" --disable-web-security"
+
+# Movements
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../.."
+alias h="cd $HOME"
+
