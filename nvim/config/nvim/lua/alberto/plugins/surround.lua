b68780c789272dcad0f878c9f287ac0df4f68a23
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-03-06 09:47:33 +0100

    docs(nvim): add help comments for nvim-surround

--- nvim/config/nvim/lua/alberto/plugins/surround.lua
+++ nvim/config/nvim/lua/alberto/plugins/surround.lua
@@ -1,4 +1,18 @@
 return {
 	"kylechui/nvim-surround",
-	config = true,
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({})
+	end,
 }
+
+--     Old text                    Command         New text
+-- --------------------------------------------------------------------------------
+--     surr*ound_words             ysiw)           (surround_words)
+--     *make strings               ys$"            "make strings"
+--     [delete ar*ound me!]        ds]             delete around me!
+--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+--     'change quot*es'            cs'"            "change quotes"
+--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--     delete(functi*on calls)     dsf             function calls
