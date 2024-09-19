0cbbd40129227e6438d8b92614c0034b9c624d9b
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-09-19 08:15:42 +0200

    feat(mas): remove unsplash wallpapers

--- app-store/install.sh
+++ app-store/install.sh
@@ -2,7 +2,7 @@ if test "$(uname)" = "Darwin"; then
 	# Tomito
 	mas install 1526042938
 	# Unsplash Wallpapers
-	mas install 1284863847
+	# mas install 1284863847
 	# DemoPRO
 	mas install 1384206666
 fi
