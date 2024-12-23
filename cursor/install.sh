1bc26fc466c1be39510d6ea41e04c9ae825b2776
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-12-23 10:35:53 +0100

    fix(Cursor): keybindings link

--- cursor/install.sh
+++ cursor/install.sh
@@ -2,15 +2,15 @@
 CURSOR_USER_PATH="$HOME/Library/Application Support/Cursor/User"
 DOTFILES_CURSOR_USER_PATH="$HOME/.dotfiles/plugins/dotfiles-alberto-chamorro/cursor/User"
 SETTINGS_FILENAME="settings.json"
-SHORTCUTS_FILENAME="shortcuts.json"
+KEYBINDINGS_FILENAME="keybindings.json"
 
 # Create the directory if it doesn't exist
 mkdir -p "$CURSOR_USER_PATH"
 
 # Remove the application files if they exist
 rm -rf "$CURSOR_USER_PATH/${SETTINGS_FILENAME}"
-rm -rf "$CURSOR_USER_PATH/${SHORTCUTS_FILENAME}"
+rm -rf "$CURSOR_USER_PATH/${KEYBINDINGS_FILENAME}"
 
 # Link files
 ln -s $DOTFILES_CURSOR_USER_PATH/${SETTINGS_FILENAME} "$CURSOR_USER_PATH/${SETTINGS_FILENAME}"
-ln -s $DOTFILES_CURSOR_USER_PATH/${SHORTCUTS_FILENAME} "$CURSOR_USER_PATH/${SHORTCUTS_FILENAME}"
+ln -s $DOTFILES_CURSOR_USER_PATH/${KEYBINDINGS_FILENAME} "$CURSOR_USER_PATH/${KEYBINDINGS_FILENAME}"
