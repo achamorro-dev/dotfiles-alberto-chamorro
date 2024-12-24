79867b884a0e229ed72a3d336da8128844203b62
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-12-24 10:27:03 +0100

    feat(zsh): add zsh plugins for autocompletions and suggestions

---
+++ zsh/_zsh.zsh
@@ -0,0 +1,11 @@
+# ZSH plugins
+source /opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+  autoload -Uz compinit
+  compinit
+fi
--- zsh/install.sh
+++ zsh/install.sh
@@ -3,3 +3,7 @@ WORKSPACE_DIR=$HOME/workspace
 if [ ! -d $WORKSPACE_DIR ];then
     mkdir $WORKSPACE_DIR
 fi
+
+chmod go-w '/opt/homebrew/share'
+chmod -R go-w '/opt/homebrew/share/zsh'
+
