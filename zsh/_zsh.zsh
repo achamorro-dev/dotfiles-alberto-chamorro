22cd47b9804c4ec4b3053b9a8df162006d631592
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-01-07 08:15:30 +0100

    feat(zsh): remove zsh-autocomplete because it has an aggressive command history

--- zsh/_zsh.zsh
+++ zsh/_zsh.zsh
@@ -1,5 +1,5 @@
 # ZSH plugins
-source /opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+# source /opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
