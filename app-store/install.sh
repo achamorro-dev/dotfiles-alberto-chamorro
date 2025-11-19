5c5b6eb9dc6db3e7823667d557852b24d7477368
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-11-19 15:58:10 +0100

    feat(app-store): remove Tomito

--- app-store/install.sh
+++ app-store/install.sh
@@ -1,13 +1,9 @@
 if test "$(uname)" = "Darwin"; then
-	read -p "¿Estás usando tu cuenta personal de App Store? (y/n): " personal_account
-	if [ "$personal_account" = "y" ]; then
-		# Tomito
-		mas install 1526042938
-		# Xcode
-		mas install 497799835
-		# Unsplash Wallpapers
-		# mas install 1284863847
-		# DemoPRO
-		mas install 1384206666
-	fi
+  read -p "¿Estás usando tu cuenta personal de App Store? (y/n): " personal_account
+  if [ "$personal_account" = "y" ]; then
+    # Xcode
+    mas install 497799835
+    # DemoPRO
+    mas install 1384206666
+  fi
 fi
